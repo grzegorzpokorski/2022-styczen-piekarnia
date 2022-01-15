bf5838b2cf0be9286db10f225f42b120f7cc32e4
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-01-15 22:22:04 +0100

    dodanie galerii:

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import 'bootstrap';
+import 'glightbox/dist/js/glightbox';
+import GLightbox from 'glightbox';
+
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
+});
